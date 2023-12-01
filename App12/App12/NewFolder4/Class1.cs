1f1ded6262ef94467238736bada8d6dffeae8a57
Alumno <Alumno@LAB1509-25>
2023-12-01 15:23:10 -0500

    Agregar archivos de proyecto.

---
+++ App12/App12/NewFolder4/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App12.NewFolder4
+{
+    internal class Class1
+    {
+    }
+}
---
+++ App12/App12/View/Vista.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App12.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Vista : ContentPage
+    {
+        public Vista()
+        {
+            InitializeComponent();
+        }
+    }
+}
